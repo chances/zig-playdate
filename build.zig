7f8374c78dd8062691a6a99bd8ab289cbfcac15e
Chance Snow <git@chancesnow.me>
2022-08-17 18:37:02 -0500

    Enhance Playdate SDK path discovery

--- build.zig
+++ build.zig
@@ -1,11 +1,22 @@
+const std = @import("std");
+const mem = std.mem;
 const builtin = @import("builtin");
-const Builder = @import("std").build.Builder;
-const CrossTarget = @import("std").zig.CrossTarget;
-const arm = @import("std").Target.arm;
+const string = []const u8;
+const Builder = std.build.Builder;
+const CrossTarget = std.zig.CrossTarget;
+const Target = std.Target;
+const arm = std.Target.arm;
 
 pub fn build(b: *Builder) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("pdex", "src/main.zig");
+
+    // Get path to the Playdate SDK
+    const sdkPath = getPlaydateSdkPath(allocator);
+    if (sdkPath == null) std.debug.print("Could not find path to Playdate SDK! Define PLAYDATE_SDK_PATH in your environment.", .{});
 
     lib.setTarget(.{
         .cpu_arch = .arm,
@@ -13,13 +24,30 @@ pub fn build(b: *Builder) !void {
         .abi = .eabi,
         .cpu_features_add = arm.cpu.cortex_m7.features,
     });
-    lib.setLinkerScriptPath(.{ .path = getPlaydateSdkPath() + "/C_API/buildsupport/link_map.ld" });
+    const linkerScriptPath = std.fs.path.join(allocator, &[_]string{
+        sdkPath.?, "C_API", "buildsupport", "link_map.ld",
+    }) catch unreachable;
+    lib.setLinkerScriptPath(.{ .path = linkerScriptPath });
     lib.linkLibC();
     lib.setBuildMode(mode);
     lib.install();
 }
 
-fn getPlaydateSdkPath() []const u8 {
-    const os = @import("std").os;
-    return try os.getenv("PLAYDATE_SDK_PATH");
+fn getPlaydateSdkPath(allocator: mem.Allocator) ?string {
+    const ChildProcess = std.ChildProcess;
+    const maxInt = std.math.maxInt;
+
+    return std.process.getEnvVarOwned(allocator, "PLAYDATE_SDK_PATH") catch |err| switch (err) {
+        error.EnvironmentVariableNotFound => {
+            if (builtin.os.tag == .windows) return null;
+            const args = [_]string{ "sh", "-c", "\"egrep '^\\s*SDKRoot' ~/.Playdate/config | head -n 1 | cut -c9-\"" };
+            const proc = ChildProcess.init(args.items, allocator) catch unreachable;
+            const status = proc.spawnAndWait() catch return null;
+            std.debug.assert(status == .{ .Exited = 0 });
+            if (proc.stdout) |stdout| {
+                return stdout.readToEndAlloc(allocator, maxInt(u64)) catch return null;
+            } else return null;
+        },
+        else => return null,
+    };
 }
